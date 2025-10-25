33375b08cbaa35103fe850cf9cb3bf7bf54058b1
Shady Youssef <shadymedhat98@gmail.com>
2025-10-25 23:42:07 +0300

    fixed vercel deployments with older version

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,13 @@
-
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import theme from "./theme";
 import Navbar from "./_components/Navbar/Navbar";
-import { ThemeProvider } from "@mui/material";
+import ThemeRegistry from "./_components/ThemeRegistry/ThemeRegistry";
 
 const poppins = Poppins({
   variable: "--font-poppins-sans",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
-
-
 
 export const metadata = {
   title: "Admin Panel",
@@ -22,13 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} `}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-          <Navbar/>
-        {children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+      <body className={poppins.variable}>
+        <ThemeRegistry>
+          <Navbar />
+          {children}
+        </ThemeRegistry>
       </body>
     </html>
   );
